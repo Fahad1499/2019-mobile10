ef2b5a6ddd14e0fa21b38fb40c9159baa361eeb7
Wahyu Hidayah <spongebob.krabby@gmail.com>
2019-11-01 14:14:14 +0700

    Percobaan SkorBasket

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
@@ -1,4 +1,44 @@
 package id.ac.polinema.skorbasket.viewmodels;
 
-public class SharedScore {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+public class SharedScore extends AndroidViewModel {
+    private MutableLiveData<Integer> scoreHome;
+    private MutableLiveData<Integer> scoreVisitor;
+    private MutableLiveData<Boolean> winner;
+
+    public SharedScore(@NonNull Application application) {
+        super(application);
+        scoreHome = new MutableLiveData<>();
+        scoreVisitor = new MutableLiveData<>();
+        winner = new MutableLiveData<>();
+    }
+
+    public void setScoreHome(Integer score){
+        scoreHome.setValue(score);
+    }
+
+    public MutableLiveData<Integer> getScoreHome(){
+        return scoreHome;
+    }
+
+    public void setScoreVisitor(Integer score){
+        scoreVisitor.setValue(score);
+    }
+
+    public MutableLiveData<Integer> getScoreVisitor(){
+        return scoreVisitor;
+    }
+
+    public void setWinner(Boolean result){
+        winner.setValue(result);
+    }
+
+    public MutableLiveData<Boolean> getWinner(){
+        return winner;
+    }
 }
